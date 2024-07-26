66c2a27187529bcb93cbbdbefb44975cf33245a3
Leonardo Cintra <leonardo.ncintra@outlook.com>
2024-07-26 12:09:10 -0300

    :pencil: add readme

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Restaurante
 
-Projeto Leonardo
+Bibliotecas que serão compartilhadas com o projeto de restaurante.
+
+## Proposito
+
+Vários "sites" irão usar a mesma lib para consulta e tratamento dos dados de um pedido de restaurante.
+
+
+# NPM
+
+https://www.npmjs.com/package/restaurante
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -16,3 +16,40 @@ export interface IItem {
   tipoItem: IItemTipo;
   tipoItemId: number;
 }
+
+interface IItemConfiguracoes {
+  id: number;
+  descricao: string;
+}
+
+interface IConfiguracoes {
+  id: number;
+  quantidade: number;
+  item_tipo: IItemConfiguracoes;
+}
+
+interface IConfiguracoesData {
+  data: IConfiguracoes[];
+}
+
+export interface IMarmitex {
+  id: number;
+  descricao: string;
+  ativo: boolean;
+  preco: number;
+  configuracoes: IConfiguracoesData;
+}
+
+export interface ITipoMarmitex {
+  id: number;
+  descricao: string;
+  restauranteId: number;
+}
+
+export interface ICardapio {
+  id: number;
+  data_cardapio: Date;
+  items: IItem;
+}
+
+
